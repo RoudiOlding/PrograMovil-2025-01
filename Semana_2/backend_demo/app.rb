05fd61532bcaaada28a12fd69aa690ed96568612
RoudiOlding <roudiolding040504@gmail.com>
2025-04-08 08:18:31 -0500

    Configurando el acceso al servidor desde ip publicas. Así compañeros podrán acceder desde sus máquinas hacia mi IP.

--- Semana_2/backend_demo/app.rb
+++ Semana_2/backend_demo/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 set :public_folder, 'public'
-set :bind, '0.0.0.0' #
-set :port, 4567 #
+set :bind, '0.0.0.0' # para que puedan acceder al servidor poniendo mi ip
+set :port, 4567
 
 get '/' do
     'Attack on Titan >> Hellsing'
