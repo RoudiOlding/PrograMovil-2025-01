f89554593541153d6fe23e66d5d3182cc590a51f
RoudiOlding <roudiolding040504@gmail.com>
2025-05-02 07:26:48 -0500

    roudi: actualizando los códigos según la clase que me perdí los últimos 20 minutos

--- ulima_quiz/lib/pages/sign_in/sign_in_page.dart
+++ ulima_quiz/lib/pages/sign_in/sign_in_page.dart
@@ -5,12 +5,10 @@ import 'sign_in_controller.dart';
 class SignInPage extends StatelessWidget {
   SignInController control = Get.put(SignInController());
 
-  SignInPage({super.key});
-
   Widget _form(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width * 0.7,
-      padding: const EdgeInsets.all(15),
+      padding: EdgeInsets.all(15),
       decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.primaryFixed,
           border: Border.all(
@@ -19,70 +17,72 @@ class SignInPage extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            padding: const EdgeInsets.all(15),
-            child: Column(children: [
-              const Text('Ingresa tu información'),
-              const SizedBox(
-                height: 15,
-              ),
-              const TextField(
-                obscureText: true, // Esto oculta el texto ingresado
-                decoration: InputDecoration(
-                  hintText: 'Usuario',
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                  prefixIcon:
-                      Icon(Icons.person), // Aquí agregamos el ícono de usuario
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const TextField(
-                obscureText: true, // Esto oculta el texto ingresado
-                decoration: InputDecoration(
-                  hintText: 'Contraseña',
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                  prefixIcon: Icon(Icons.lock), // Ícono de candado
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Acción del botón
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize:
-                      const Size(double.infinity, 40), // 100% del ancho
-                  backgroundColor:
-                      const Color(0xFFFF7F2A), // Color de fondo personalizado
-                  foregroundColor: Colors.white, // Color del texto blanco
-                  shape: const RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.zero, // Sin redondeo en las esquinas
+            padding: EdgeInsets.only(
+              top: 0, // Padding superior
+              bottom: 0, // Padding inferior
+              left: 25, // Padding izquierdo
+              right: 25, // Padding derecho
+            ),
+            child: Column(
+              children: [
+                Text('Ingresa Esta Información'),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Usuario',
+                    hintText: 'Usuario',
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    prefixIcon: Icon(
+                        Icons.person), // Aquí agregamos el ícono de usuario
+                    //border: OutlineInputBorder(),
                   ),
                 ),
-                child: Text('INGRESAR'),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const Row(
-                children: [
-                  Text('No tienes una cuenta, '),
-                  Text(
-                    'creala aquí',
-                    style: TextStyle(
-                      fontWeight: FontWeight
-                          .bold, // Esto hace que el texto sea en negrita
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  obscureText: true, // Esto oculta el texto ingresado
+                  decoration: InputDecoration(
+                    hintText: 'Contraseña',
+                    prefixIcon: Icon(Icons.lock), // Ícono de candado
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción del botón
+                  },
+                  child: Text('INGRESAR'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40), // 100% del ancho
+                    backgroundColor:
+                        Color(0xFFFF7F2A), // Color de fondo personalizado
+                    foregroundColor: Colors.white, // Color del texto blanco
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.zero, // Sin redondeo en las esquinas
                     ),
-                  )
-                ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('No tienes una cuenta, '),
+              Text(
+                'creala aquí',
+                style: TextStyle(
+                  fontWeight:
+                      FontWeight.bold, // Esto hace que el texto sea en negrita
+                ),
               )
-            ]),
+            ],
           )
         ],
       ),
@@ -93,7 +93,7 @@ class SignInPage extends StatelessWidget {
     return SafeArea(
         child: Column(
       children: [
-        const SizedBox(
+        SizedBox(
           height: 50,
         ),
         Row(
@@ -109,13 +109,12 @@ class SignInPage extends StatelessWidget {
                         color: Theme.of(context).colorScheme.primaryFixed,
                         width: 10.0)),
                 child: ClipOval(
-                  child: Image.asset(
-                      'assets/images/zapatillas_correr.jpg', // usamos la definifica en el .yaml
-                      fit: BoxFit.cover),
+                  child:
+                      Image.asset('assets/images/g23.png', fit: BoxFit.cover),
                 )),
           ],
         ),
-        const SizedBox(
+        SizedBox(
           height: 40,
         ),
         _form(context)
@@ -127,10 +126,27 @@ class SignInPage extends StatelessWidget {
     return Stack(
       children: [
         Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: _layer1(context),
+            top: 0, // Coloca el widget en la parte inferior
+            left: 0, // Alinea al inicio del eje horizontal
+            right: 0, // Alinea al final del eje horizontal
+            child: _layer1(context)),
+        Positioned(
+          bottom: 40, // Coloca el widget a 40 píxeles del fondo
+          left: 0, // Alinea al inicio del eje horizontal
+          right: 0, // Alinea al final del eje horizontal
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center, // Centra el contenido
+            children: [
+              Text('Olvidaste tu contraseña?, '),
+              Text(
+                'recupérala aquí',
+                style: TextStyle(
+                  fontWeight:
+                      FontWeight.bold, // Hace que el texto sea en negrita
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
